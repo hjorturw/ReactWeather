79f66f307a1cbeaa3c5c7040b1ba86d4c0e76d5a
Hjörtur Waltersson <hjorturw@gmail.com>
2016-10-14 14:40:18 +0000

    Add error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=metric';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c803f71521228b78e29ee2605d2bc27e&units=metric';
 
 module.exports = {
   getTemp: function (location) {
